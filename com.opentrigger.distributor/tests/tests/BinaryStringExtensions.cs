f076835955505fa5cb71b9e785953b3c1d6af503
FF <f+git@fida.biz>
2016-03-17 01:13:29 +0100

    wip

---
+++ com.opentrigger.distributor/tests/tests/BinaryStringExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using com.opentrigger.distributord;
+using NUnit.Framework;
+
+namespace com.opentrigger.tests
+{
+    [TestFixture]
+    public class BinaryStringExtensions
+    {
+        [Test]
+        public void TryNunit()
+        {
+            Debug.WriteLine(new byte[] { 0xAA }.ToHexString());
+            Assert.IsTrue("FF".ToBytes() == new byte[] {0xFF});
+            Assert.IsTrue(new byte[] {0xAA}.ToHexString() == "AA");
+        }
+    }
+}
