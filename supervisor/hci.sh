4d610bef439f87d49f4ce06fc53e55a8a7dcb9d7
Flo <f+git@fida.biz>
2016-07-24 23:01:06 +0200

    hci setup startup check

--- supervisor/hci.sh
+++ supervisor/hci.sh
@@ -10,7 +10,30 @@ _term() {
 }
 trap _term SIGINT SIGTERM
 
+command_missing () {
+    ! type "$1" &> /dev/null ;
+}
+
+required="grep otraw2q mosquitto_pub hciconfig hcitool hcidump pkill"
+missing=""
+for c in $required ; do
+	if command_missing $c ; then
+		echo "missing: $c"
+		missing="$missing $c"
+	fi
+done
+
+if [[ ! -z "$missing" ]]; then
+	echo "dependencies missing, setup is probably not complete..."
+	exit 1
+fi
+
 hcidevices=$(hciconfig | grep -Po '^hci[0-9](?=:)')
+
+if [[ -z "$hcidevices" ]]; then
+	echo "no devices found"
+	exit 1
+fi
 
 for hcidevice in $hcidevices ; do
 	hcimac=$(hciconfig $hcidevice | grep -m1 -Po '(?<=Address: )[0-F:]+')
