ce6c0519168e245c7ae7eaf6cd73eb37f831044b
FF <f+git@fida.biz>
2016-07-24 15:55:24 +0200

    move default configuration to own class

---
+++ com.opentrigger.distributor/cli/DefaultConfig.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using com.opentrigger.distributord;
+
+namespace com.opentrigger.cli.distributord
+{
+    class DefaultConfig
+    {
+        public DistributorConfig GetDefaultDistributorConfig()
+        {
+            var connection = string.Intern("tcp://fserv");
+            var includeMacs_TokenCube = new[] { "F1:11:11:11:10:12", "CA:9F:0A:00:66:20" };
+            var includeMacs_WhiteButtons = new[] { "0C:F3:EE:00:2D:00", "0C:F3:EE:00:2E:7D" };
+
+            var excludeMacs = new[] { "54:60:09:39:F0:55" }; // annoying things...
+            int skip = 0;
+
+            var config = new DistributorConfig
+            {
+                QueueDistributorConfigs = new[]{
+                    new QueueDistributorConfig {
+                        Connection = connection,
+                        Skip = skip,
+                        IncludedMacs = includeMacs_WhiteButtons,
+                        ExcludedMacs = excludeMacs,
+                    },
+                    new QueueDistributorConfig {
+                        Connection = connection,
+                        Skip = skip,
+                        IncludedMacs = includeMacs_TokenCube,
+                        UniqueIdentifier = UniqueIdentifier.Nrf51MacAndTokenCubePir,
+                        Distance = 100,
+                    },
+                    //new QueueDistributorConfig {
+                    //    Connection = connection,
+                    //    Skip = skip,
+                    //    UniqueIdentifier = UniqueIdentifier.MacAndAdvertisingData,
+                    //    ReleaseTopic = null,
+                    //    TriggerTopic = "/opentrigger/signals/sensors",
+                    //    Distance = 0,
+                    //    IncludedMacs = includeMacs_TokenCube,
+                    //    ExcludedMacs = excludeMacs,
+                    //},
+                },
+                RunParallel = true,
+            };
+            return config;
+        }
+    }
+}
--- com.opentrigger.distributor/cli/DistributorEntry.cs
+++ com.opentrigger.distributor/cli/DistributorEntry.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using com.opentrigger.cli.distributord;
 
 namespace com.opentrigger.distributord
 {
@@ -11,12 +12,14 @@ namespace com.opentrigger.distributord
     {
         static void Main(string[] args)
         {
-            // 1 = Trigger/Release
+            // 0 = Quiet
+            // 1 = Trigger/Release, print config
             // 2 = Json
             // 3 = Take, Skip, PublishFails
-            // 4 = Duplicate
+            // 4 = Duplicates
             // 5 = Excluded
-            int verbosity = 2 + args.Count(arg => arg == "-v" || arg == "--verbose") - args.Count(arg => arg == "-q" || arg == "--quiet");
+
+            int verbosity = 0 + args.Count(arg => arg == "-v" || arg == "--verbose") - args.Count(arg => arg == "-q" || arg == "--quiet");
 
             // --roundtrip 172.17.17.136 --verbose
             var roundtripIndex = Array.IndexOf(args,"--roundtrip");
@@ -29,44 +32,7 @@ namespace com.opentrigger.distributord
                 new RoundtripTime(server, padding, verbosity);
             }
 
-            DistributorConfig config;
-
-            var connection = string.Intern("tcp://pi3");
-            var includeMacs_TokenCube = new[] { "F1:11:11:11:10:12", "CA:9F:0A:00:66:20" };
-            var includeMacs_WhiteButtons = new[] { "0C:F3:EE:00:2D:00", "0C:F3:EE:00:2E:7D" };
-
-            var excludeMacs = new[] {"54:60:09:39:F0:55"}; // annoying things...
-            int skip = 0;
-
-            config = new DistributorConfig {
-                QueueDistributorConfigs = new[]{
-                    new QueueDistributorConfig {
-                        Connection = connection,
-                        Skip = skip,
-                        IncludedMacs = includeMacs_WhiteButtons,
-                        ExcludedMacs = excludeMacs,
-                    },
-                    new QueueDistributorConfig {
-                        Connection = connection,
-                        Skip = skip,
-                        IncludedMacs = includeMacs_TokenCube,
-                        UniqueIdentifier = UniqueIdentifier.Nrf51MacAndTokenCubePir,
-                        Distance = 100,
-                    },
-                    //new QueueDistributorConfig {
-                    //    Connection = connection,
-                    //    Skip = skip,
-                    //    UniqueIdentifier = UniqueIdentifier.MacAndAdvertisingData,
-                    //    ReleaseTopic = null,
-                    //    TriggerTopic = "/opentrigger/signals/sensors",
-                    //    Distance = 0,
-                    //    IncludedMacs = includeMacs_TokenCube,
-                    //    ExcludedMacs = excludeMacs,
-                    //},
-                },
-                Verbosity = verbosity,
-                RunParallel = true,
-            };
+            var config = new DefaultConfig().GetDefaultDistributorConfig();
 
             var configIndex = Array.IndexOf(args, "-c");
             if (configIndex == -1) configIndex = Array.IndexOf(args, "--config");
@@ -76,7 +42,9 @@ namespace com.opentrigger.distributord
                 {
                     var configFile = args.ElementAtOrDefault(configIndex + 1);
                     if(string.IsNullOrWhiteSpace(configFile)) throw new Exception("Invalid config file name");
+                    if(!System.IO.File.Exists(configFile)) throw new Exception("Missing config file - " + configFile);
                     var configContent = System.IO.File.ReadAllText(configFile, Encoding.UTF8);
+                    if(string.IsNullOrWhiteSpace(configContent)) throw new Exception("Empty config file - " + configFile);
                     config = configContent.Deserialize<DistributorConfig>();
                 }
                 catch (Exception)
@@ -87,7 +55,9 @@ namespace com.opentrigger.distributord
                 
             }
 
-            if(verbosity > 0) Console.WriteLine(config.Serialize());
+            config.Verbosity += verbosity;
+
+            if(config.Verbosity > 0) Console.WriteLine(config.Serialize());
 
             var distributors = config.QueueDistributorConfigs.Select(queueDistributorConfig => new QueueDistributor(queueDistributorConfig,config.Verbosity)).ToList();
             if (config.RunParallel) while (true)
