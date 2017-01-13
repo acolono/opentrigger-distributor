96bc017c729de9acc041d6d406b29c91ad7fb05e
FF <f+git@fida.biz>
2017-01-14 00:47:11 +0100

    ledblink

--- com.opentrigger.distributor/lib/Plugins/LedBlinkPlugin.cs
+++ com.opentrigger.distributor/lib/Plugins/LedBlinkPlugin.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using CoAP;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace com.opentrigger.distributord.Plugins
 {
@@ -14,18 +16,40 @@ namespace com.opentrigger.distributord.Plugins
         public void Start(string[] cmdlineArgs)
         {
             if(cmdlineArgs.Length != 2) return;
-            var argIndex = Array.IndexOf(cmdlineArgs, "--blinkled");
+            var argIndex = Array.IndexOf(cmdlineArgs, "--ledblink");
             if(argIndex == -1) return;
-            var addr = cmdlineArgs[argIndex + 1];
-            var uri = new Uri($"coap://{addr}/led/RGB");
-            var timeout = 5000;
 
-            var greenRequest = new Request(Method.PUT) {URI = uri, PayloadString = "r=0&g=255&b=0" };
-            var offRequest = new Request(Method.PUT) {URI = uri, PayloadString = "r=0&g=0&b=0" };
-            greenRequest.Send();
-            var greenresponse = greenRequest.WaitForResponse(timeout);
-            if(greenresponse == null) throw new TimeoutException("no response");
-            offRequest.Send().WaitForResponse(timeout);
+            var jsonResponse = new JsonResponse();
+
+            try
+            {
+                jsonResponse.UniqueIdentifier = cmdlineArgs[argIndex + 1];
+                var uri = new Uri($"coap://{jsonResponse.UniqueIdentifier}/led/RGB");
+                var timeout = 5000;
+
+                var greenRequest = new Request(Method.PUT) { URI = uri, PayloadString = "r=0&g=255&b=0" };
+                var offRequest = new Request(Method.PUT) { URI = uri, PayloadString = "r=0&g=0&b=0" };
+                greenRequest.Send();
+                var greenresponse = greenRequest.WaitForResponse(timeout);
+                if (greenresponse == null) throw new TimeoutException("green - no response");
+                var offresponse = offRequest.Send().WaitForResponse(timeout);
+                if (offresponse == null) throw new TimeoutException("off - no response");
+                jsonResponse.Success = true;
+            }
+            catch (Exception e)
+            {
+                jsonResponse.Success = false;
+                jsonResponse.Error = e.Message;
+            }
+            Console.WriteLine(jsonResponse.Serialize(false));
+            Environment.Exit(jsonResponse.Success ? 0 : 1);
+        }
+
+        public class JsonResponse
+        {
+            public bool Success { get; set; }
+            public string Error { get; set; }
+            public string UniqueIdentifier { get; set; }
         }
     }
 }
