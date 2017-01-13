d77733730ec8ddea62614a99491aaa63c6e4686f
FF <f+git@fida.biz>
2017-01-14 00:01:52 +0100

    reflection tests

---
+++ com.opentrigger.distributor/lib/Plugins/PluginBase.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace com.opentrigger.distributord.Plugins
+{
+    public interface IPlugin
+    {
+        void Start(string[] cmdlineArgs);
+    }
+
+    public class DummyPlugin : IPlugin
+    {
+        public void Start(string[] cmdlineArgs)
+        {
+            if (cmdlineArgs.Contains("--dummy"))
+            {
+                Console.WriteLine("DummyPlugin");
+            }
+        }
+    }
+
+    public static class Plugins
+    {
+        public static readonly Dictionary<Type,IPlugin> ActivatedPlugins = new Dictionary<Type, IPlugin>();
+        public static void StartPlugins(string[] cmdlineArgs)
+        {
+            var allClasses = AppDomain.CurrentDomain.GetAssemblies().SelectMany(a => a.GetTypes()).Where(t => t.IsClass);
+            var pluginInterface = typeof(IPlugin);
+            foreach (var type in allClasses)
+            {
+                var interfaces = type.GetInterfaces();
+                if (interfaces.Contains(pluginInterface))
+                {
+                    try
+                    {
+                        var plugin = Activator.CreateInstance(type) as IPlugin;
+                        if (plugin != null)
+                        {
+                            plugin.Start(cmdlineArgs);
+                            ActivatedPlugins.Add(type, plugin);
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        throw new Exception($"Plugin {type.FullName} Activation failed", ex);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ com.opentrigger.distributor/tests/PluginTests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using com.opentrigger.distributord.Plugins;
+using NUnit.Core;
+using NUnit.Framework;
+
+namespace com.opentrigger.tests
+{
+    [TestFixture]
+    public class PluginTests
+    {
+        [Test]
+        public void BuiltinDummy()
+        {
+            var dummy = typeof(DummyPlugin);
+            Plugins.StartPlugins(new []{"--dummy"});
+            Console.WriteLine(string.Join(", ", Plugins.ActivatedPlugins.Keys.Select(k => k.FullName)));
+
+            var activeDummy = Plugins.ActivatedPlugins[dummy] as DummyPlugin;
+            Assert.IsNotNull(activeDummy);
+        }
+
+        private class FailingPlugin : IPlugin
+        {
+            public void Start(string[] cmdlineArgs)
+            {
+                throw new Exception("die");
+            }
+        }
+
+        [Test]
+        public void Failing()
+        {
+            var name = typeof(FailingPlugin).FullName;
+            var cnt = 0;
+            try
+            {
+                Plugins.StartPlugins(new string[] { });
+            }
+            catch (Exception e)
+            {
+                if (e.Message.Contains(name))
+                {
+                    Assert.AreEqual(e.InnerException?.Message,"die");
+                    cnt++;
+                }
+                else
+                {
+                    throw;
+                }
+                
+            }
+            Assert.AreEqual(cnt,1);
+        }
+    }
+}
